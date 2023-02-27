e9a93008859d1c8b566efa19c3a6e03a8acd545c
Gabriel <126514525+GabrielGarcia12345678910@users.noreply.github.com>
2023-02-27 13:43:42 -0600

    Add files via upload

---
+++ M1/S3/java/01SaludoApp.java
@@ -0,0 +1,8 @@
+import java.io.*;
+class saludoApp  
+  {
+   public statioc void main(String[] args)
+     {
+      System.out.print("Grupo 230 PROGRAMASIÓN");
+     }
+   }
---
+++ M1/S3/java/02Suma.java
@@ -0,0 +1,11 @@
+import java.io.*;
+class suma
+  {
+   public static void main(string[] args)
+     {
+      int a = 10;
+      int b = 5;
+      int suma  = a + b;
+      System.out.primit("la SUMA es:"+suma);
+     }
+   }
